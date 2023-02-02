1db274d41961403d9b722755262cc659430d329b
er48730 <er48730@ubt-uni.net>
2023-02-02 14:58:59 +0100

    Added first Controllers

---
+++ CinemaManagementSystem/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using CinemaManagementSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace CinemaManagementSystem.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
